860bf67d7759eaada750ae1e184a38520c3d180e
Drew Noakes <git@drewnoakes.com>
2016-05-31 00:06:16 +0100

    Underscore prefixes for fields.

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -12,29 +12,29 @@ namespace StyleSnooper
 {
     public partial class Window1
     {
-        private readonly Style bracketStyle, elementStyle, quotesStyle, textStyle, attributeStyle, commentStyle;
-        private readonly Microsoft.Win32.OpenFileDialog openFileDialog;
+        private readonly Style _bracketStyle, _elementStyle, _quotesStyle, _textStyle, _attributeStyle, _commentStyle;
+        private readonly Microsoft.Win32.OpenFileDialog _openFileDialog;
 
         public Window1()
         {
             this.InitializeComponent();
 
             // get syntax coloring styles
-            bracketStyle = Resources["BracketStyle"] as Style;
-            elementStyle = Resources["ElementStyle"] as Style;
-            quotesStyle = Resources["QuotesStyle"] as Style;
-            textStyle = Resources["TextStyle"] as Style;
-            attributeStyle = Resources["AttributeStyle"] as Style;
-            commentStyle = Resources["CommentStyle"] as Style;
+            _bracketStyle = Resources["BracketStyle"] as Style;
+            _elementStyle = Resources["ElementStyle"] as Style;
+            _quotesStyle = Resources["QuotesStyle"] as Style;
+            _textStyle = Resources["TextStyle"] as Style;
+            _attributeStyle = Resources["AttributeStyle"] as Style;
+            _commentStyle = Resources["CommentStyle"] as Style;
 
             // start out by looking at Button
             CollectionViewSource.GetDefaultView(this.ElementTypes).MoveCurrentTo(typeof(Button));
 
             // create the file open dialog
-            openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.CheckFileExists = true;
-            openFileDialog.Multiselect = false;
-            openFileDialog.Filter = "Assemblies (*.exe;*.dll)|*.exe;*.dll";
+            _openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            _openFileDialog.CheckFileExists = true;
+            _openFileDialog.Multiselect = false;
+            _openFileDialog.Filter = "Assemblies (*.exe;*.dll)|*.exe;*.dll";
         }
 
         private void SandAndFire(object sender, EventArgs e)
@@ -42,17 +42,17 @@ namespace StyleSnooper
             GlassHelper.TryExtendGlassFrame(this, styleTextBox.Margin);
         }
 
-        private Type[] elementTypes;
+        private Type[] _elementTypes;
         public Type[] ElementTypes
         {
             get
             {
-                if (elementTypes == null)
+                if (_elementTypes == null)
                 {
-                    elementTypes = GetFrameworkElemenetTypesFromAssembly(typeof(FrameworkElement).Assembly);
+                    _elementTypes = GetFrameworkElemenetTypesFromAssembly(typeof(FrameworkElement).Assembly);
                 }
 
-                return elementTypes;
+                return _elementTypes;
             }
         }
 
@@ -86,19 +86,19 @@ namespace StyleSnooper
 
         private void OnLoadClick(object sender, RoutedEventArgs e)
         {
-            if (openFileDialog.ShowDialog(this) == true)
+            if (_openFileDialog.ShowDialog(this) == true)
             {
                 try
                 {
-                    AsmName.Text = openFileDialog.FileName;
-                    Type[] types = GetFrameworkElemenetTypesFromAssembly(Assembly.LoadFile(openFileDialog.FileName));
+                    AsmName.Text = _openFileDialog.FileName;
+                    Type[] types = GetFrameworkElemenetTypesFromAssembly(Assembly.LoadFile(_openFileDialog.FileName));
                     if (types.Length == 0)
                     {
                         MessageBox.Show("Assembly does not contain any compatible types.");
                     }
                     else
                     {
-                        elementTypes = types;
+                        _elementTypes = types;
 
                         BindingExpression exp = BindingOperations.GetBindingExpression(typeComboBox, ComboBox.ItemsSourceProperty);
                         exp.UpdateTarget();
@@ -191,39 +191,39 @@ namespace StyleSnooper
                         if (reader.IsStartElement()) // opening tag, e.g. <Button
                         {
                             // indentation
-                            AddRun(paragraph, textStyle, new string(' ', indent * 4));
+                            AddRun(paragraph, _textStyle, new string(' ', indent * 4));
 
-                            AddRun(paragraph, bracketStyle, "<");
-                            AddRun(paragraph, elementStyle, reader.Name);
+                            AddRun(paragraph, _bracketStyle, "<");
+                            AddRun(paragraph, _elementStyle, reader.Name);
                             if (reader.HasAttributes)
                             {
                                 // write tag attributes
                                 while (reader.MoveToNextAttribute())
                                 {
-                                    AddRun(paragraph, attributeStyle, " " + reader.Name);
-                                    AddRun(paragraph, bracketStyle, "=");
-                                    AddRun(paragraph, quotesStyle, "\"");
+                                    AddRun(paragraph, _attributeStyle, " " + reader.Name);
+                                    AddRun(paragraph, _bracketStyle, "=");
+                                    AddRun(paragraph, _quotesStyle, "\"");
                                     if (reader.Name == "TargetType") // target type fix - should use the Type MarkupExtension
                                     {
-                                        AddRun(paragraph, textStyle, "{x:Type " + reader.Value + "}");
+                                        AddRun(paragraph, _textStyle, "{x:Type " + reader.Value + "}");
                                     }
                                     else
                                     {
-                                        AddRun(paragraph, textStyle, reader.Value);
+                                        AddRun(paragraph, _textStyle, reader.Value);
                                     }
-                                    AddRun(paragraph, quotesStyle, "\"");
+                                    AddRun(paragraph, _quotesStyle, "\"");
                                 }
                                 reader.MoveToElement();
                             }
                             if (reader.IsEmptyElement) // empty tag, e.g. <Button />
                             {
-                                AddRun(paragraph, bracketStyle, " />");
+                                AddRun(paragraph, _bracketStyle, " />");
                                 paragraph.Inlines.Add(new LineBreak());
                                 --indent;
                             }
                             else // non-empty tag, e.g. <Button>
                             {
-                                AddRun(paragraph, bracketStyle, ">");
+                                AddRun(paragraph, _bracketStyle, ">");
                                 paragraph.Inlines.Add(new LineBreak());
                             }
 
@@ -232,17 +232,17 @@ namespace StyleSnooper
                         else // closing tag, e.g. </Button>
                         {
                             // indentation
-                            AddRun(paragraph, textStyle, new string(' ', indent * 4));
+                            AddRun(paragraph, _textStyle, new string(' ', indent * 4));
 
                             // text content of a tag, e.g. the text "Do This" in <Button>Do This</Button>
                             if (reader.NodeType == XmlNodeType.Text)
                             {
-                                AddRun(paragraph, textStyle, reader.ReadContentAsString());
+                                AddRun(paragraph, _textStyle, reader.ReadContentAsString());
                             }
 
-                            AddRun(paragraph, bracketStyle, "</");
-                            AddRun(paragraph, elementStyle, reader.Name);
-                            AddRun(paragraph, bracketStyle, ">");
+                            AddRun(paragraph, _bracketStyle, "</");
+                            AddRun(paragraph, _elementStyle, reader.Name);
+                            AddRun(paragraph, _bracketStyle, ">");
                             paragraph.Inlines.Add(new LineBreak());
 
                             --indent;
